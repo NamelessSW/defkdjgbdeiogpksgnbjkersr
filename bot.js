6ecac70bc45f0ce1ef2853ac830af19de301bbf6
NamelessSW <41346517+NamelessSW@users.noreply.github.com>
2018-07-25 01:25:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1483,39 +1483,4 @@ if(!message.channel.guild) return;
          message.channel.sendEmbed(Embed11)
     }
 });
-//Music
-const YTDL = require("ytdl-core");
-function play(connection, message) {
-    var server = servers[message.guild.id];
-    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
-    server.queue.shift();
-    server.dispatcher.on("end", function() {
-        if(server.queue[0]) play(connection);
-        else connection.disconnect();
-    })
-}
-var servers = {};
-client.on('message' , async (message) => {
-       if(message.content.startsWith(prefix + "play")) {
-              let args = message.content.split(" ").slice(1);
-    //play
-    if (!args[0]) {
-         message.channel.send("Please specify a link");
-         return
-    }
-    if(!message.member.voiceChannel) {
-        message.channel.sned("I think it may work better if you are in a voice channel!");
-    }
-    if(!servers[message.guild.id]) servers[message.guild.id] = {
-        queue: []
-    }
-    var server = servers[message.guild.id];
-    server.queue.push(args[0]);
-    message.channel.send("Your song of choice is on the queue.` ")
-    if(!message.member.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
-        play(connection, message);
-    })
-}
-
-});
 client.login(process.env.BOT_TOKEN);
