72ab4ee904ded4cf9eb6fc03d35deca7cf6afb77
NamelessSW <41346517+NamelessSW@users.noreply.github.com>
2018-07-25 04:21:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -446,6 +446,7 @@ let embed = new Discord.RichEmbed()
 .addField('     **❧ +help-setup-AutoRole ➺ Setup Auto Role  :asterisk:** ','**════════════**') //Done
 .addField('     **❧ +help-setup-tickets ➺ Setup tickets :notepad_spiral:** ','**════════════**') //Done
 .addField('     **❧ +help-setup-verify ➺ Setup Verify :bust_in_silhouette: ** ','**════════════**') //Done
+.addField('     **❧ +help-setup-logs ➺ Setup bot logs ::newspaper: ** ','**════════════**') //Done
 .addField("**:red_circle:  Nameless Support Discord :tools:   **","**-https://discord.gg/atk3A2C**") 
 .addField("**:pushpin:  Don't forget to vote for Nameless Bot :heartbeat:**","**-https://discordbots.org/bot/465993722342014986/vote**")
 .setColor('#7d2dbe')
@@ -1582,4 +1583,66 @@ client.on('messageDelete', message => {
      channel.send({embed:embed});
 
 });
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if(cmd === `${prefix}kick`){
+
+    //!kick @daeshan askin for it
+
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Can't find user!");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Soory User bot you dont have enghout permision to use this command");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("~Kick~")
+    .setColor("#e56b00")
+    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Kicked In", message.channel)
+    .addField("Tiime", message.createdAt)
+    .addField("Reason", kReason);
+
+    
+
+    message.guild.member(kUser).kick(kReason);
+    message.channel.sendEmbed(kickEmbed);
+
+    return;
+  }
+
+  if(cmd === `${prefix}ban`){
+
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("Can't find user!");
+    let bReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("Soory User bot you dont have enghout permision to use this command");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription("~Ban~")
+    .setColor("#bc0000")
+    .addField("Banned User", `${bUser} with ID ${bUser.id}`)
+    .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Banned In", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", bReason);
+
+    let incidentchannel = message.guild.channels.find(`name`, "incidents");
+    if(!incidentchannel) return message.channel.send("Can't find incidents channel.");
+
+    message.guild.member(bUser).ban(bReason);
+    message.channel.sendEmbed(banEmbed);
+
+
+    return;
+  }
 client.login(process.env.BOT_TOKEN);
