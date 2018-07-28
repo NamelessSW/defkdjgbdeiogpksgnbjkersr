d4db832c66793dd36c09a2134a6f7263e26f4ffb
NamelessSW <41346517+NamelessSW@users.noreply.github.com>
2018-07-28 15:18:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1724,16 +1724,4 @@ client.on("message", message => {
       //})
       //});
 
-const DBL = require("dblapi.js");
-const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ2NTk5MzcyMjM0MjAxNDk4NiIsImJvdCI6dHJ1ZSwiaWF0IjoxNTMyNzc5MTc3fQ.8QEBfBKuGDWkSEhGLmC6H3Zq7WW5ptEhgdANga2tJbo', client);
-
-// Optional events
-dbl.on('posted', () => {
-  console.log('Server count posted!');
-})
-
-dbl.on('error', e => {
- console.log(`Oops! ${e}`);
-})
-
 client.login(process.env.BOT_TOKEN);
