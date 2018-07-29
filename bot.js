6eb20f8820419fe8e86c9dd7f836e72974594959
NamelessSW <41346517+NamelessSW@users.noreply.github.com>
2018-07-29 16:26:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1109,13 +1109,20 @@ client.on('message', message => {
 }
 });
 //Roles
-client.on('message', message => {
-    if (message.content === '+roles') {
-        var roles = message.guild.roles.map(roles => `${roles.name}, `).join(' ')
-        const embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .addField('**Roles**:',`**[${roles}]**`)
-        message.channel.sendEmbed(embed);
+var AsciiTable = require('ascii-data-table').default
+client.on('message', message =>{
+
+    if(message.content == "+roles"){
+        var 
+        ros=message.guild.roles.size,
+        data = [['Rank', 'RoleName']]
+        for(let i =0;i<ros;i++){
+            if(message.guild.roles.array()[i].id !== message.guild.id){
+         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
+        }}
+        let res = AsciiTable.table(data)
+
+        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
     }
 });
 
